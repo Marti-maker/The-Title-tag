42df9c79d79bcb9291997f137cdc29e32ec1c3d3
Martin <77927455+Marti-maker@users.noreply.github.com>
2023-01-08 01:17:38 +0200

    2
    2

--- src/js/app.js
+++ src/js/app.js
@@ -2,11 +2,12 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-
-   
-      document.title="One new message!" 
-   // 6000 milliseconds = 6 seconds
-  
+  setTimeout(
+    function(){ 
+      document.title="One new message" 
+    }, 
+    3000  // 6000 milliseconds = 6 seconds
+  ); 
 
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
