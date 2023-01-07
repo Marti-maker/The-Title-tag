09bc860d50b6db5c63013f94fe09b1a57029d34c
Martin <77927455+Marti-maker@users.noreply.github.com>
2023-01-08 01:01:11 +0200

    Ops i think i did it
    ;X

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,12 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+  setTimeout(
+    function(){ 
+      document.title="One new message!" 
+    }, 
+    6000  // 6000 milliseconds = 6 seconds
+  ); 
 
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
